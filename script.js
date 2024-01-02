c4e3bc1cb66fed08bebcdf6eb6e5723eec0efcc8
Immanuel Olaoye <olaoye939@gmail.com>
2024-01-01 22:00:41 -0600

    first commit!

---
+++ script.js
@@ -0,0 +1,61 @@
+// alert('Hello World!');
+
+console.log("This is logging!");  // This is an log
+console.warn("This is a warning!"); // This is an warning
+console.error('This is an error!') // This is an error
+
+
+// Variables
+
+const age = 30;
+// age = 31; Throws an error
+
+// let is mutable
+// let x = 1;
+// x = 2, is valid
+
+// let n; is valid ✅
+// const n; is a syntax error ❌
+
+
+// Primative Data Types
+
+// String, Numbers, Boolean, null, undefined, * symbols *
+
+
+const my_name = 'John';
+const number = 30;
+const rating = 45;
+const isCool = true;
+const x = null;
+
+console.log(typeof number) // -> number
+console.log(typeof my_name) // -> string
+
+
+// Strings
+
+
+// Template literals
+
+console.log(`My name is ${my_name} and I am ${number}`)
+
+
+// String Methods
+
+const s = 'Hello World';
+console.log(s.length);
+console.log(s.split);
+
+
+// Arrays - variables that hold multiple values
+
+const numbers = new Array(1, 2, 3, 4, 5);
+
+
+console.log(numbers)
+
+
+
+
+
